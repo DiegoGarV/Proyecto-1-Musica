587d079c73371ec5714815487c18241013dfb00f
rodrigoajmac <ajm22279@uvg.edu.gt>
2026-08-01 11:48:25 -0600

    Update bajo.py
    Línea de bajo , pendiente determinar el número de repeticiones. Linea transcrita de https://www.songsterr.com/a/wsa/verve-bitter-sweet-symphony-tab-s21055t3

--- bajo.py
+++ bajo.py
@@ -1,26 +1,34 @@
 from music import *
 
+repet = 5
 
 class Bajo(object):
 
-    def __init__(self):
+    def __init__(self, tempo=120, numerador=4, denominador=4):
         self.nombre = "Bajo"
-        self.instrumento = 32  # Acoustic Bass
+        self.instrumento = ELECTRIC_BASS
         self.canal = 0
+        self.tempo = tempo                  # BPM
+        self.numerador = numerador          # Ej. 4 para 4/4
+        self.denominador = denominador      # Ej. 4 para 4/4
 
     def crear_frase(self):
-        # 0.0 significa que empieza desde el inicio de la canción.
         frase = Phrase(0.0)
 
-        notas = [
-            # Colocar notas aquí.
+        # Configuración de Tempo y Compás
+        frase.setTempo(self.tempo)
+        frase.setNumerator(self.numerador)
+        frase.setDenominator(self.denominador)
 
-        ]
+        notas = [
+            E2, B1, REST,                   # Compás 1
+            A1, REST, A1, A1, A1, REST, A1  # Compás 2
+        ]*repet
 
         duraciones = [
-            # Colocar duraciones aquí.
-
-        ]
+            2.0, 1.5, 0.5,                      # Compás 1
+            1.0, 0.5, 0.25, 0.25, 1.0, 0.5, 0.5  # Compás 2
+        ]*repet
 
         if len(notas) != len(duraciones):
             raise ValueError(
@@ -33,12 +41,13 @@ class Bajo(object):
         return frase
 
     def crear_parte(self):
-        parte = Part(
-            self.nombre,
-            self.instrumento,
-            self.canal
-        )
-
+        parte = Part(self.nombre, self.instrumento, self.canal)
         parte.addPhrase(self.crear_frase())
-
         return parte
+
+
+
+mi_bajo = Bajo(tempo=83, numerador=4, denominador=4)
+parte_bajo = mi_bajo.crear_parte()
+
+Play.midi(parte_bajo)
