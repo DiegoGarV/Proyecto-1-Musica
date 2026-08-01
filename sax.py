6fd9aa4463d0ee3a81f7e58ab15eaee8774d433e
DiegoGarV <107023284+DiegoGarV@users.noreply.github.com>
2026-07-31 22:47:09 -0600

    saxo part

--- sax.py
+++ sax.py
@@ -1,31 +1,71 @@
 from music import *
 
+# C, F y G son Sharp
 
 class Saxofon(object):
 
     def __init__(self):
         self.nombre = "Saxofon"
-        self.instrumento = 65  # Alto Sax
+        self.instrumento = ALTO_SAX
         self.canal = 3
 
     def crear_frase(self):
         frase = Phrase(0.0)
 
-        notas = [
-            # Colocar notas aquí.
+        notasCoro = [
+            REST, ES4, GS4, ES4, REST, DS4, FS4, DS4,
+            REST, FS4, B4, FS4, REST, FS4, AS4, FS4
         ]
 
-        duraciones = [
-            # Colocar duraciones aquí.
-        ]
+        notas = ([
+            ES4, FS4,
+            FS4, FS4,
+            ES4, FS4,
+            FS4, FS4, AS4, CS5, ES5,
+            GS5,
+            GS5,
+            GS5,
+            GS5,
+            ES4, FS4,
+            FS4, FS4,
+            ES4, FS4,
+            FS4, FS4,
+            REST, ES5, GS5, ES5, FS5, DS5, FS5,
+            B5, FS5, B5, AS5, FS5, AS5,
+            ES5, GS5, ES5, FS5, DS5, FS5,
+            B5, FS5, B5, AS5, FS5, AS5,
+            REST, ES5, GS5, ES5, FS5, DS5, FS5,
+            B5, FS5, B5, AS5, FS5, AS5,
+            ES5, GS5, ES5, FS5, DS5, FS5,
+            B5, FS5, B5, AS5, FS5, AS5
+        ] + notasCoro * 4 + [
+            CS5, GS4,
+            A4, FS4,
+            CS5, GS4,
+            A4, FS4,
+            CS5, GS4
+        ])
 
-        if len(notas) != len(duraciones):
+        blancas = [HN]
+        negras = [QN]
+        corcheas = [EN]
+        redondas = [WN]
+        durationTot = (
+            blancas * 7 + corcheas * 4 + redondas * 4 + blancas * 8 + 
+            corcheas * 6 + negras + corcheas * 2 + negras + corcheas * 2 + negras + 
+            corcheas * 5 + negras + corcheas * 2 + negras + corcheas * 2 + negras + 
+            corcheas * 6 + negras + corcheas * 2 + negras + corcheas * 2 + negras + 
+            corcheas * 5 + negras + corcheas * 2 + negras + corcheas * 2 + negras +
+            corcheas * 64 + blancas * 10 
+        )
+
+        if len(notas) != len(durationTot):
             raise ValueError(
                 "El saxofon debe tener la misma cantidad de notas y duraciones."
             )
 
         if len(notas) > 0:
-            frase.addNoteList(notas, duraciones)
+            frase.addNoteList(notas, durationTot)
 
         return frase
 
