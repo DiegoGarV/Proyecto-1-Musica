6b17c2b474a2354f6c4e4fd8fe12262a7393e0b6
DiegoGarV <107023284+DiegoGarV@users.noreply.github.com>
2026-07-31 23:19:09 -0600

    violoncello part

--- violoncello.py
+++ violoncello.py
@@ -5,27 +5,47 @@ class Violoncello(object):
 
     def __init__(self):
         self.nombre = "Violoncello"
-        self.instrumento = 42  # Cello
+        self.instrumento = CELLO
         self.canal = 4
 
     def crear_frase(self):
         frase = Phrase(0.0)
 
-        notas = [
-            # Colocar notas aquí.
+        notasCoro = [
+            D3, A2,
+            E3, B2
         ]
 
-        duraciones = [
-            # Colocar duraciones aquí.
-        ]
+        notas = ([
+            E3, D3
+        ] + notasCoro * 13 + [
+            D3, A2,
+            REST, GS2, B2, GS2, A2, FS2, A2,
+            D3, A2, D3, CS3, A2, CS3,
+            REST, GS2, B2, GS2, A2, FS2, A2,
+            D3, A2, D3, CS3, A2, CS3,
+            REST, GS2, B2, GS2, A2, FS2, A2
+        ])
 
-        if len(notas) != len(duraciones):
+        blancas = [HN]
+        negras = [QN]
+        corcheas = [EN]
+        durationTot = (
+            blancas * 56 + 
+            corcheas * 6 + negras + 
+            corcheas * 2 + negras + corcheas * 2 + negras + 
+            corcheas * 6 + negras + 
+            corcheas * 2 + negras + corcheas * 2 + negras + 
+            corcheas * 6 + negras
+        )
+
+        if len(notas) != len(durationTot):
             raise ValueError(
                 "El violoncello debe tener la misma cantidad de notas y duraciones."
             )
 
         if len(notas) > 0:
-            frase.addNoteList(notas, duraciones)
+            frase.addNoteList(notas, durationTot)
 
         return frase
 
