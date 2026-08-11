289f603c7ec2cb0217b8c72b9518c2e9c2d314af
rodrigoajmac <ajm22279@uvg.edu.gt>
2026-08-10 18:22:31 -0600

    Update CancionProyecto1.py

--- Cancion Propia/CancionProyecto1.py
+++ Cancion Propia/CancionProyecto1.py
@@ -4,8 +4,8 @@ start = 3.0
 
 mainTempo = 85
 
-# Esta melodia debe usar LA (A), SI (B), DO# (C#), 
-# RE (D), MI (E), FA# (F#), SOL (G#)
+# Esta melodía debe usar LA (A), SI (B), DO# (C#), 
+# RE (D), MI (E), FA# (F#), SOL# (G#)
 
 # ---------------------- Melody ----------------------
 melody = Phrase(start) 
@@ -54,22 +54,52 @@ durationMelody = (negras * 48 + corcheas * 32 + coroDuration * 4 + corcheas * 32
 melody.addNoteList(notesMelody, durationMelody)
 
 # ---------------------- Bass ----------------------
-bass = Phrase(start) 
+# El segundo compás inicia 4 tiempos después de 'start'
+bass = Phrase(start + 4.0) 
+
+# Definición de la secuencia de bajo
+pattern = [
+    # ((E2, 2), (E2, 0.5), (C#2, 0.5), (B1, 1), (A1, 4)) - Repetición 1
+    (E2, 2.0), (E2, 0.5), (CS2, 0.5), (B1, 1.0), (A1, 4.0),
+    # ((E2, 2), (E2, 0.5), (C#2, 0.5), (B1, 1), (A1, 4)) - Repetición 2
+    (E2, 2.0), (E2, 0.5), (CS2, 0.5), (B1, 1.0), (A1, 4.0),
+    # ((E2, 2), (E2, 0.5), (D2, 0.5), (C#2, 1), (B1, 4))
+    (E2, 2.0), (E2, 0.5), (D2, 0.5), (CS2, 1.0), (B1, 4.0),
+    # ((E2, 2), (D2, 0.5), (C#2, 0.5), (B1, 1), (A1, 4))
+    (E2, 2.0), (D2, 0.5), (CS2, 0.5), (B1, 1.0), (A1, 4.0)
+]
 
 notesBass = []
-
 durationBass = []
+
+# Duración requerida para el bajo (duración total menos el primer compás)
+target_duration = sum(durationMelody) - 4.0
+current_duration = 0.0
+
+# Se repite el patrón hasta cubrir toda la canción
+while current_duration < target_duration:
+    for note, dur in pattern:
+        if current_duration >= target_duration:
+            break
+        notesBass.append(note)
+        durationBass.append(dur)
+        current_duration += dur
 
 bass.addNoteList(notesBass, durationBass)
 
-# ---------------------- Instrumento ----------------------
-piano = Part("Piano", PIANO, 0)
-piano.addPhrase(melody)
-piano.addPhrase(bass)
+# ---------------------- Instrumentos ----------------------
+# Piano para la melodía (Canal 0)
+pianoPart = Part("Piano", PIANO, 0)
+pianoPart.addPhrase(melody)
+
+# Bajo Eléctrico para el bajo (Canal 1)
+bassPart = Part("Bajo Electrico", ELECTRIC_BASS, 1)
+bassPart.addPhrase(bass)
 
 # ---------------------- Partitura ----------------------
 score = Score(mainTempo)
-score.addPart(piano)
+score.addPart(pianoPart)
+score.addPart(bassPart)
 
 Play.midi(score)
 
